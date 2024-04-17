74386ad01260cb9d8e2adb2b705353d430649e01
Harshal <harshalnangare2020@gmail.com>
2024-04-17 15:06:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// index.js or App.js
+
+import './index.css';
+
+
+
+import DargAndDropToDo  from './todo';
+
+function App() {
+  return (
+      <DargAndDropToDo />                                      
+  );
+}
+
+export default App;
